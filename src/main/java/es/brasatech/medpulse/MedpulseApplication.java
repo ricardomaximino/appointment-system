79eb8b281d20efc73a6f98c5a5c19f956af41cc3
Ricardo Maximino <ricardo.maximino@innobadev.altia.es>
2026-05-24 17:32:37 +0200

    Fist commit

---
+++ src/main/java/es/brasatech/medpulse/MedpulseApplication.java
@@ -0,0 +1,13 @@
+package es.brasatech.medpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedpulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedpulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/brasatech/medpulse/MedpulseApplicationTests.java
@@ -0,0 +1,13 @@
+package es.brasatech.medpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedpulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
